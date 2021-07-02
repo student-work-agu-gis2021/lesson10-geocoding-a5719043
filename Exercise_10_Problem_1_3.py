5992c5a5277d1eb77eb492066acc35f5fa3ce842
a5719043 <82347415+a5719043@users.noreply.github.com>
2021-07-02 08:03:06 +0000

    report how many people live within 1.5 km distance from each shopping center

--- Exercise_10_Problem_1_3.py
+++ Exercise_10_Problem_1_3.py
@@ -130,7 +130,7 @@ join = gpd.sjoin(geodata, pop, how="inner", op="intersects")
 # YOUR CODE HERE 11 to report how many people live within 1.5 km distance from each shopping center
 grouped = join.groupby('name')
 for key, group in grouped:
-    print('store: ', key,"\n", 'population:', sum(group['asukkaita']))
+    print('store: ', key,"\n", 'population:', sum(group['asukkaita'])) 
 # **Reflections:**
 #     
 # - How challenging did you find problems 1-3 (on scale to 1-5), and why?
